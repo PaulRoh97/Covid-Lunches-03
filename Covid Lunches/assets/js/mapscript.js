f677df0cbb64776a640a8627c2fff93ed8adaec3
F-421 <qq67378@umbc.edu>
2021-03-22 21:41:05 -0400

    Added marker on default location

--- Covid Lunches/assets/js/mapscript.js
+++ Covid Lunches/assets/js/mapscript.js
@@ -22,10 +22,31 @@ function callMapAPI() {
 /*For our embedded Google map */
 function myMap() {
 
+    /*TODO: data query the profile's default location's*/
+    const default_location = {lat: samplePerson.latitude_default, lng: samplePerson.longitude_default};
+
+    /*Map currently looks at pcikup location */
     var mapProp = {
-        center: new google.maps.LatLng(51.508742, -0.120850),
+        center: default_location,
         zoom: 15,
     };
-
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+    var infowindow = new google.maps.InfoWindow({
+        content: '<div id="content"> <h2>' +
+            samplePerson.location_label + '</h2> </divs>',
+    });
+
+    /*See the location*/
+    const marker = new google.maps.Marker({
+        position: default_location,
+        map: map,
+        animation: google.maps.Animation.DROP,
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+    });
+
+    
 }
---
+++ Covid Lunches/assets/js/sampleProfileInfo.js
@@ -0,0 +1,7 @@
+// Quick fake person to test before database is fully implemented
+var samplePerson = {
+    username : "Bob Bob",
+    latitude_default: 39.34885819161449, 
+    longitude_default: -76.51970863089325,
+    location_label: "Elmwood Elementary School"
+};
