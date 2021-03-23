9f2c51ece7dd4f69c5e6e0d4462823b6affb81b3
F-421 <qq67378@umbc.edu>
2021-03-23 12:36:47 -0400

    added javascript to call for profile pic

---
+++ Covid Lunches/assets/js/profilepic.js
@@ -0,0 +1,20 @@
+// JavaScript source code
+
+/* the src of the image is the profile picture in the database*/
+function get_profile_pic() {
+
+    var src = "assets/img/chefs/chefs-1.jpg";
+
+    /*TODO: don't do if we can't find what we look for*/
+    if (1) {
+        document.getElementById("Profile_Pic").src = src;
+    }
+
+}
+
+/* change the profile picture to the uploaded image
+ */
+function change_profile_pic( new_img) {
+
+    /*TODO: perform checks on image before making it the src*/
+}
