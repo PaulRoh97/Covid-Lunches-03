67b262ab8b5d6a9f8651ddc3a426b7a5337157b1
Maura Choudhary <maurac1@umbc.edu>
2021-03-25 13:03:13 -0400

    bootstrap with nunjucks express set up

---
+++ Covid Lunches/express/express-bootstrap/app.js
@@ -0,0 +1,36 @@
+let express = require('express')
+let http = require('http')
+let nunjucks = require('nunjucks')
+let path = require('path')
+let sassMiddleware = require('node-sass-middleware')
+
+let app = express()
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+})
+
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'bootstrap'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true
+}))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(req, res, next) {
+    let  data = {
+      content: 'Hello world!',
+      title: 'Bootstrap example'
+    }
+  
+    // this renders 'data' into Nunjucks template 'index.njk'
+    res.render('index.njk', data)
+})
+
+let server = http.createServer(app)
+server.listen('3000', () => {
+    console.log('Listening on port 3000')
+})
