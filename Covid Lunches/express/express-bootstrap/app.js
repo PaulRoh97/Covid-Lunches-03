0e7ea0aae3005537301be5248334ff5208af9995
Maura Choudhary <maurac1@umbc.edu>
2021-03-28 14:09:11 -0400

    cleaned up express framework

--- Covid Lunches/express/express-bootstrap/app.js
+++ Covid Lunches/express/express-bootstrap/app.js
@@ -7,30 +7,30 @@ let sassMiddleware = require('node-sass-middleware')
 let app = express()
 
 nunjucks.configure('views', {
-    autoescape: true,
-    express: app
+  autoescape: true,
+  express: app
 })
 
 app.use(sassMiddleware({
-    src: path.join(__dirname, 'bootstrap'),
-    dest: path.join(__dirname, 'public'),
-    indentedSyntax: true, // true = .sass and false = .scss
-    sourceMap: true
+  src: path.join(__dirname, 'bootstrap'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true
 }))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res, next) {
-    let  data = {
-      content: 'Hello world!',
-      title: 'Bootstrap example'
-    }
-  
-    // this renders 'data' into Nunjucks template 'index.njk'
-    res.render('index.njk', data)
+  let  data = {
+    content: 'Hello world!',
+    title: 'Bootstrap example'
+  }
+
+  res.render('index.njk', data)
 })
 
 let server = http.createServer(app)
+
 server.listen('3000', () => {
-    console.log('Listening on port 3000')
+  console.log('Listening on port 3000')
 })
