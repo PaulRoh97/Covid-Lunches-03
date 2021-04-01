57550de550fe946cc31c9ff58af5d840ea85d124
Maura Choudhary <maurac1@umbc.edu>
2021-03-31 22:20:39 -0400

    added comments

--- Covid Lunches/express/express-bootstrap/routes/index.js
+++ Covid Lunches/express/express-bootstrap/routes/index.js
@@ -1,4 +1,6 @@
 module.exports = {
+    //Title: gethomepage
+    //Purpose: Renders the starting page of the project. Tests that the homepage renders properly
     gethomepage: (req, res) =>{
         let  data = {
             content: 'Hello world!',
