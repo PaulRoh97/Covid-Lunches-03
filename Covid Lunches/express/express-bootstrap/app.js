3fa3a4db24bf1e0005c41836e25e4d5a3b1c0156
Adam Sachsel <asachse1@umbc.edu>
2021-03-31 20:38:19 -0400

    Added the files to use a general password config. Currently storing DB credentials. + a .gitignore

--- Covid Lunches/express/express-bootstrap/app.js
+++ Covid Lunches/express/express-bootstrap/app.js
@@ -8,14 +8,16 @@ let sassMiddleware = require('node-sass-middleware')
 let app = express()
 const {gethomepage} = require('./routes/index')
 
+var options = require('./options.js');
+
 
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 const db = mysql.createConnection ({
-  host: 'softwaredev.cfbh0dw0st0e.us-east-1.rds.amazonaws.com',
-  user: 'admin',
-  password: '14AWGblu!!',
-  database: 'socka'
+  host: options.storageConfig.host,
+  user: options.storageConfig.user,
+  password: options.storageConfig.password,
+  database: options.storageConfig.db
 });
 
 // connect to database
