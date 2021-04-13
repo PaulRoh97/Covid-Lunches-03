08086b24bad9cfb1f3dcc6bd594adc3bfc92fca1
asachse1 <42878671+asachse1@users.noreply.github.com>
2021-04-13 04:37:37 -0400

    Got the model for "Accounts" working. Workin on profile picture integration.

--- Covid Lunches/express/express-bootstrap/app.js
+++ Covid Lunches/express/express-bootstrap/app.js
@@ -7,6 +7,8 @@ let path = require('path')
 let sassMiddleware = require('node-sass-middleware')
 let covid = require('./routes/covid')
 let Sequelize = require('sequelize')
+let db = require('./models')
+db.sequelize.sync()
 
 let app = express()
 // const {gethomepage} = require('./routes/index')
@@ -14,54 +16,6 @@ let app = express()
 
 var options = require('./options.js');
 
-
-// create connection to database
-// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
-var db = new Sequelize('socka', options.storageConfig.user, options.storageConfig.password, {
-  host: options.storageConfig.host,
-  dialect: 'mysql',
-
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
-
-db
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-
-
-//Testing DB Table creation/Sync
-const Account = db.define('accounts', { accountID: Sequelize.INTEGER, name: Sequelize.STRING, profilePic: Sequelize.STRING, testData: Sequelize.STRING});
-
-db.sync({ force: true })
-  .then(() => {
-    console.log('Database & Tables created!');
-
-  });
-
-  
-//Creating "Account" in the new table
-db.sync({ force: true })
-  .then(() => {
-    console.log('Database & tables created!')
-
-    Account.bulkCreate([
-      { accountID: 1, name: 'Adam', profilePic: '/public/img/pumpple.jpg'}
-    ]).then(function() {
-      return Account.findAll()
-    }).then(function(accounts) {
-      console.log(accounts)
-    })
-  })
 
 nunjucks.configure('views', {
   autoescape: true,
@@ -82,8 +36,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', covid)
 
 
-
 let server = http.createServer(app)
+
 
 server.listen('3000', () => {
   console.log('Listening on port 3000')
