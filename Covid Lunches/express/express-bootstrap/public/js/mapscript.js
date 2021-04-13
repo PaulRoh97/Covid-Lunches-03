208422bde11a9bfbedc0a9c49705f6e092b5695e
F-421 <qq67378@umbc.edu>
2021-04-13 16:44:30 -0400

    Added testing for multiple locations

--- Covid Lunches/express/express-bootstrap/public/js/mapscript.js
+++ Covid Lunches/express/express-bootstrap/public/js/mapscript.js
@@ -25,7 +25,6 @@ function myMap() {
 
     /*TODO: data query the profile's default location's*/
     const default_location = { lat: 39.34885819161449, lng: -76.51970863089325 };
-    //39.015975363512396, -76.67026638671018
     var test_lat_index = 0;
 
     /*Map currently looks at pcikup location */
@@ -42,17 +41,28 @@ function myMap() {
         animation: google.maps.Animation.DROP,
     });
 
+    console.log("created map at marker location");
+
+    /*Test functionality: change marker to a list of testing locations on click*/
     google.maps.event.addListener(marker, 'click', function () {
 
         /*change lat long for testing*/
-        marker.setMap(null);
-        var new_location = { lat: 39.015975363512396, lng: -76.67026638671018 };
-        new_marker = new google.maps.Marker({
-            position: new_location,
-            map: map,
-            animation: google.maps.Animation.DROP,
-        });
+        testLat = testLocations[test_lat_index][1];
+        testLng = testLocations[test_lat_index][2];
+
+        /*test out of range: default location time!*/
+        if (testLat < -90)  { testLat = 39.34885819161449; testLng = -76.51970863089325;}
+        if (testLat > 90)   { testLat = 39.34885819161449; testLng = -76.51970863089325; }
+        if (testLng < -180) { testLat = 39.34885819161449; testLng = -76.51970863089325; }
+        if (testLng > 180)  { testLat = 39.34885819161449; testLng = -76.51970863089325; }
+
+        var new_location = { lat: testLat, lng: testLng};
+
+        marker.setPosition(new_location);
         map.setCenter(new_location);
+        console.log("Have new marker at location: " + testLocations[test_lat_index][0]);
+
+        test_lat_index = (test_lat_index + 1) % testLocations.length;
     });
 
 
