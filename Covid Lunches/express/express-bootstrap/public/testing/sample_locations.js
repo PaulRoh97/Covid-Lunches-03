d4409f002a54b93fcd4943ae74e73f79d4f2a3fb
F-421 <qq67378@umbc.edu>
2021-04-13 16:44:30 -0400

    Added testing for multiple locations

---
+++ Covid Lunches/express/express-bootstrap/public/testing/sample_locations.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+var testLocations = [["Sample school 1", 39.015975363512396, -76.67026638671018],
+                     ["Catonsville High", 39.260205218309785, -76.73157793392849],
+                     ["Fail Test 1", -91, -91],
+                     ["Fail Test 2", 91, 0],
+                     ["Fail Test 3", 0, -181],
+                     ["Fail Test 4", 0, 181]];
