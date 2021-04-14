1ced3067c36d2671d5b513b9c563266cfdb2d7fe
F-421 <qq67378@umbc.edu>
2021-04-14 13:06:07 -0400

    List of inputs to check

---
+++ Covid Lunches/express/express-bootstrap/public/testing/testGuestCodes.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+var testGuestCodes = ["12345678", "1234567890123456",
+
+                     /*character checking*/
+                     "12345678!",
+                     "12345678a",          
+
+                     /*boundaries of length limit*/
+                     "",
+                     "12345678901234567",
+                     "1234567",
+
+                     /*SQL injection testing 
+                     [for fun, even though I have no idea what an exmaple looks like]*/
+                     "1 or 1=1;--"
+                     ];
