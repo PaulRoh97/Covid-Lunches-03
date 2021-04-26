4a447d143188be1eecaa59bdf4d9b3705a0933df
F-421 <qq67378@umbc.edu>
2021-04-26 17:33:44 -0400

    Adding to table

---
+++ Covid Lunches/express/express-bootstrap/public/js/schedulescript.js
@@ -0,0 +1,37 @@
+
+var schedule_table = [[3, "02/01/20",  "2:00 am", 0]];
+
+/*Add in the contents for the schedule table */
+function addTableContents(){
+
+    /*Add to table based on how many entrees in schedule*/
+    var table_size = schedule_table.length
+    for(i = 0; i < table_size; i++){
+
+        console.log('Adding the ' + (i + 1) + 'th entree to table');
+
+        /*Should be able to access entrees the same way */
+        var entry_id = schedule_table[i][0];
+        var entry_date = schedule_table[i][1];
+        var entry_time = schedule_table[i][2];
+        var entry_status = schedule_table[i][3];
+
+        document.write("<tr>");
+        document.write("<td>"+entry_id+"</td>");
+        document.write("<td>"+entry_date+"</td>");
+        document.write("<td>"+entry_time+"</td>");
+
+        /*status: write a dot based on value*/
+        if(entry_status == 0){
+            document.write('<td><span class="dot complete"></span></td>');
+        }
+        else if(entry_status == 1){
+            document.write('<td><span class="dot incomplete"></span></td>');
+        }
+        else{
+             document.write('<td><span class="dot\"></span></td>');
+        }
+
+        document.write("/tr");
+    }
+}
