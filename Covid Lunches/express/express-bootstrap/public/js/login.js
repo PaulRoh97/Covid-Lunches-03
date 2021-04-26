cb639cd244effa0ad1364cc6fa76df699b124da3
Abel <abelalex@live.com>
2021-04-25 20:12:43 -0400

    added sign up page and updated covid.js to include routing and updated other files with minor changes

--- Covid Lunches/express/express-bootstrap/public/js/login.js
+++ Covid Lunches/express/express-bootstrap/public/js/login.js
@@ -1,97 +1,125 @@
 // get the modal
-let modal = document.getElementById("modal");
+let modal = document.getElementById("modal")
 // get the element that is not the login form
-let container = document.getElementById("container");
+let container = document.getElementById("container")
 // get the button that opens the login form
-let loginBtn = document.getElementById("login-btn");
+let loginBtn = document.getElementById("login-btn")
 // grab the login button
-let loginFormBtn = document.getElementById('login-form-btn');
+let loginFormBtn = document.getElementById('login-form-btn')
 
 // open the pop-up login form when the user clicks on the login button
 loginBtn.onclick = function () {
-    modal.style.display = "block";
+    modal.style.display = "block"
 }
 
 // close the login form when user clicks outside of it and cleans up input fields
 window.onclick = function (event) {
     if (event.target == modal) {
-        modal.style.display = "none";
-        document.getElementById('email').value = '';
-        document.getElementById('password').value = '';
-        clearError('email-alert');
-        clearError('pass-alert');
+        modal.style.display = "none"
+        document.getElementById('email').value = ''
+        document.getElementById('password').value = ''
+        clearError('email-alert')
+        clearError('pass-alert')
     }
 }
 
 // validate email and password upon login attempt
 loginFormBtn.onclick = function () {
-    validateEmail();
-    validatePassword();
+    emailValidated = validateEmail()
+    passwordValidated = validatePassword()
+
+    if (emailValidated && passwordValidated) {
+        location.href = "/inner-page"
+    }
 }
 
 // validate email address
 function validateEmail() {
+    // // grab the email input field
+    // let emailField = document.getElementById('email')
+    // let mailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    // let errorMessage = ''
+
+    // if (emailField.value.match(mailFormat)) {
+    //     clearError('email-alert')
+    //     return true
+    // } else if (emailField.value == '') {
+    //     errorMessage = 'Please enter an email address.'
+    // } else {
+    //     errorMessage = 'Invalid email address. Please try again.'
+    // }
+    // showError(errorMessage, 'email-alert', 'email')
+    // return false
     // grab the email input field
-    let emailField = document.getElementById('email');
-    let mailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    let errorMessage = '';
+    let emailField = document.getElementById('email')
+    let mailFormat = /^(([^<>()\[\]\\.,'{}?+-_!@#$%*;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    let errorMessage = ''
 
     if (emailField.value.match(mailFormat)) {
-        clearError('email-alert');
-        return true;
+        clearError('email-alert')
+        return true
     } else if (emailField.value == '') {
-        errorMessage = 'Please enter an email address.';
+        errorMessage = 'Please enter an email address.'
     } else {
-        errorMessage = 'Invalid email address. Please try again.';
+        errorMessage = 'Invalid email address. Please try again.'
     }
-    showError(errorMessage, 'email-alert', 'email');
-    return false;
+    showError(errorMessage, 'email-alert', 'email')
+    return false
 }
 
 // validate password
 function validatePassword() {
-    // grab the password input field
-    let passField = document.getElementById('password');
-    let passFormat = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
-    let errorMessage = '';
+    // // grab the password input field
+    let passField = document.getElementById('password')
+    // let passFormat = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/
+    // let errorMessage = ''
 
-    if (passField.value.match(passFormat)) {
-        clearError('pass-alert');
-    } else if (passField.value == '') {
-        errorMessage = 'Please enter a password.';
-    } else {
-        errorMessage = 'Password must be 8 to 16 characters and contain at least one numeric digit and a special character.';
+    // if (passField.value.match(passFormat)) {
+    //     clearError('pass-alert')
+    // } else if (passField.value == '') {
+    //     errorMessage = 'Please enter a password.'
+    // } else {
+    //     errorMessage = 'Password must be 8 to 16 characters and contain at least one numeric digit and a special character.'
+    // }
+    // showError(errorMessage, 'pass-alert', 'password')
+    // return true
+    if (passField.value.length >= 8) {
+        clearError('pass-alert')
+        return true
     }
-    showError(errorMessage, 'pass-alert', 'password');
-    return true;
+    let errorMessage = 'Please enter a password.',
+        errorType = 'pass-alert',
+        element = 'password'
+
+    showError(errorMessage, errorType, element)
+    return false
 }
 
 // display error message
 function showError(errorMessage, errorType, element) {
     // create a div
-    const errorDiv = document.createElement('div');
-    errorDiv.className = `${errorType} alert-danger`;
+    const errorDiv = document.createElement('div')
+    errorDiv.className = `${errorType} alert-danger`
 
     // get elements
-    const card = document.querySelector(`.${element}`);
-    const inputField = document.querySelector(`#${element}`);
+    const card = document.querySelector(`.${element}`)
+    const inputField = document.querySelector(`#${element}`)
 
     // create text node and append to div
-    errorDiv.appendChild(document.createTextNode(errorMessage));
-    errorDiv.style.color = 'red';
-    errorDiv.style.marginBottom = "10px"
+    errorDiv.appendChild(document.createTextNode(errorMessage))
+    errorDiv.style.color = 'red'
 
     // insert error message above input field
-    card.insertBefore(errorDiv, inputField);
+    card.insertBefore(errorDiv, inputField)
 
     // clear errors
     if (document.getElementsByClassName(errorType).length > 1) {
-        clearError(errorType);
+        clearError(errorType)
     }
 }
 
 function clearError(errorType) {
     if (document.getElementsByClassName(`${errorType}`).length >= 1) {
-        document.querySelector(`.${errorType}`).remove();
+        document.querySelector(`.${errorType}`).remove()
     }
 }
