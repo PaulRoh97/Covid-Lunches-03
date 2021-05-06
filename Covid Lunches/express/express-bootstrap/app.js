a205a352b5528d2cd527290c28bf905a8143e180
Maura Choudhary <maurac1@umbc.edu>
2021-05-06 12:25:53 -0400

    routes testing complete

--- Covid Lunches/express/express-bootstrap/app.js
+++ Covid Lunches/express/express-bootstrap/app.js
@@ -47,3 +47,4 @@ let server = http.createServer(app)
 server.listen('3000', () => {
   console.log('Listening on port 3000')
 })
+module.exports = server;
---
+++ Covid Lunches/express/express-bootstrap/tests/routes.test.js
@@ -0,0 +1,35 @@
+const covid = require("../routes/covid");
+
+const request = require("supertest");
+const express = require("express");
+const app = require("../app.js");
+// const app = express();
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use("/", covid);
+
+test("main route works", done => {
+    request(app)
+      .get("/")
+      .expect(200, done);
+  });
+  
+test("testing login route works", done => {
+  request(app)
+    .post("/sign-in")
+    .type("form")
+    .send({ email: "test@test.com", password: "password123" })
+    .then(() => {
+      request(app)
+        .get("/inner-page")
+        .expect(200, done);
+    });
+});
+
+test("testing sign up route works", done => {
+    request(app)
+      .post("/sign-up")
+      .type("form")
+      .send({ email: "test1@test.com", firstName: "Johnny1", lastName: "Doe1", password: "Password1@#$" })
+      .expect(302, done);
+  });
